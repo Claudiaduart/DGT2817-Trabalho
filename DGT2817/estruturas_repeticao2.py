6e57cb4cb2e8c86788ded273e9480055037a40ba
Claudiaduart <166668508+Claudiaduart@users.noreply.github.com>
2025-03-26 20:53:14 -0300

    Update estruturas_repeticao2.py

--- DGT2817/estruturas_repeticao2.py
+++ DGT2817/estruturas_repeticao2.py
@@ -4,7 +4,6 @@ texto = 'Olá, laço for.'
 # Itere sobre cada caractere do texto
 for item in texto:
     print(f'Caractere: {item}')
-
 # Itere sobre um intervalo numérico de 1 a 10
 for numero in range(1, 11):
     print(f'Número do intervalo: {numero}')
